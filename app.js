952940ad86dd96df8e12d6faa4ed71e425f0041c
azptrj-beep <azptrj@gmail.com>
2026-05-16 23:10:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,41 @@
    LESHANA-ED-SOURETH
    CLEAN APP.JS
 ========================================= */
+/* =========================
+   THEME TOGGLE CLEAN
+========================= */
+
+function toggleTheme() {
+
+  const html =
+    document.documentElement;
+
+  const current =
+    html.getAttribute("data-theme");
+
+  const next =
+    current === "light" ? "dark" : "light";
+
+  html.setAttribute("data-theme", next);
+
+  localStorage.setItem("theme", next);
+}
+
+/* LOAD THEME */
+
+window.addEventListener("DOMContentLoaded", () => {
+
+  const saved =
+    localStorage.getItem("theme");
+
+  if (saved) {
+
+    document.documentElement.setAttribute(
+      "data-theme",
+      saved
+    );
+  }
+});
 
 /* =========================================
    DARK MODE
