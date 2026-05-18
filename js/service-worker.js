0c48fe95bcd23b7d5f167c8fd0a7569a7656922c
azptrj-beep <azptrj@gmail.com>
2026-05-18 10:48:49 +0200

    Update service-worker.js

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,7 +1,18 @@
 
 
 "use strict";
-service-worker.js
-self.addEventListener('install', () => {
-  console.log('Service Worker installé');
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+
+    caches.open("soureth-v7").then(cache => {
+
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/css/style.css",
+        "/js/app.js"
+      ]);
+    })
+  );
 });
