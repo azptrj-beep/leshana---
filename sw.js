fbf07cad2a4778f7d9924def0c6a74a6800e79f6
azptrj-beep <azptrj@gmail.com>
2026-05-18 22:16:58 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = "soureth-pro-v1";
+
+const FILES = [
+  "/",
+  "/traduction.html",
+  "/css/style.css",
+  "/js/translate.js",
+  "/manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
