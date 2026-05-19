24eebb079b6002c057e0a1a89920913884f76144
azptrj-beep <azptrj@gmail.com>
2026-05-19 22:12:27 +0200

    Create mots.js

---
+++ js/mots.js
@@ -0,0 +1,11 @@
+const mots = [
+  {
+    fr: "Bonjour",
+    soureth: "Shlama",
+
+    audioH: "audio/mots/shlama_h.mp3",
+    audioF: "audio/mots/shlama_f.mp3",
+
+    image: "images/mots/shlama.jpg"
+  }
+];
