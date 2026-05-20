85760be73b5853492feb54310c988085384f6757
azptrj-beep <azptrj@gmail.com>
2026-05-20 10:59:38 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,81 @@
+
 "use strict";
 
+const CACHE = "soureth-pro-v2";
+
+const FILES = [
+
+  "/",
+  "/index.html",
+  "/alphabet.html",
+  "/voyelles.html",
+  "/ecriture.html",
+  "/revision.html",
+  "/vocal.html",
+  "/traduction.html",
+
+  "/css/style.css",
+
+  "/js/app.js",
+  "/js/translate.js",
+  "/js/writing.js",
+  "/js/quiz.js",
+
+  "/manifest.json",
+
+  "/images/logo.png"
+];
+
+// INSTALL
+self.addEventListener("install", e => {
+
+  e.waitUntil(
+
+    caches.open(CACHE)
+      .then(cache => cache.addAll(FILES))
+
+  );
+
+  self.skipWaiting();
+});
+
+// ACTIVATE
+self.addEventListener("activate", e => {
+
+  e.waitUntil(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+
+      );
+
+    })
+
+  );
+
+  self.clients.claim();
+});
+
+// FETCH
+self.addEventListener("fetch", e => {
+
+  e.respondWith(
+
+    caches.match(e.request)
+      .then(response => {
+
+        return response || fetch(e.request);
+
+      })
+
+  );
+
+});
 const CACHE = "soureth-pro-v1";
 
 const FILES = [
