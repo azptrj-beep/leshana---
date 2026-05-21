3c85b571f73cfedfa26046291448e3a09fe106e6
azptrj-beep <azptrj@gmail.com>
2026-05-21 19:48:37 +0200

    Create quiz-data.js

---
+++ js/quiz-data.js
@@ -0,0 +1,5 @@
+const quizData = [
+  { type: "image", src: "assets/images/apple.png", answer: "apple" },
+  { type: "audio", src: "assets/audio/word.mp3", answer: "word" },
+  { type: "letter", value: "ܐ", answer: "Alap" }
+];
