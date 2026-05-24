f48d0400e29dae36657861b893037bf9efcc0bdd
azptrj-beep <azptrj@gmail.com>
2026-05-24 22:42:36 +0200

    Create ui.js

---
+++ js/ui.js
@@ -0,0 +1,16 @@
+export function initUI() {
+  document.querySelectorAll(".card").forEach(card => {
+    card.addEventListener("click", (e) => {
+      const ripple = document.createElement("span");
+
+      ripple.className = "ripple";
+
+      ripple.style.left = e.offsetX + "px";
+      ripple.style.top = e.offsetY + "px";
+
+      card.appendChild(ripple);
+
+      setTimeout(() => ripple.remove(), 600);
+    });
+  });
+}
