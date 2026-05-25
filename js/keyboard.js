385125ad056287fa0b5bae42a8a09d6cb2f75c2f
azptrj-beep <azptrj@gmail.com>
2026-05-25 09:55:13 +0200

    Update keyboard.js

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,10 +1,25 @@
-
-
 "use strict";
 
-export function insertLetter(letter) {
-  const input = document.getElementById("editor");
-  if (!input) return;
+function pressKey(letter){
 
-  input.value += letter;
+  const input =
+    document.getElementById("frInput");
+
+  if(!input) return;
+
+  const start = input.selectionStart;
+  const end = input.selectionEnd;
+
+  input.value =
+    input.value.substring(0,start)
+    + letter
+    + input.value.substring(end);
+
+  input.selectionStart =
+    input.selectionEnd =
+    start + letter.length;
+
+  input.focus();
 }
+
+window.pressKey = pressKey;
