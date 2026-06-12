bcfb62cf22a3774b1fb43781c6730885611af434
azptrj-beep <azptrj@gmail.com>
2026-06-12 22:24:19 +0200

    Update keyboard.js

--- js/keyboard.js
+++ js/keyboard.js
@@ -2,21 +2,26 @@
    KEYBOARD.JS — Clavier Soureth Premium
 ============================================ */
 
-const keyboardContainer = document.querySelector(".soureth-keyboard");
-const output = document.querySelector(".soureth-writing");
+const keyboardContainer = document.getElementById("keyboard");
+const output = document.getElementById("input");
 
 if (keyboardContainer && output) {
 
-    // INSÉRER UNE LETTRE
     keyboardContainer.addEventListener("click", (e) => {
+
+        // INSÉRER UNE LETTRE
         if (e.target.classList.contains("key-btn")) {
             const letter = e.target.dataset.letter;
             output.value += letter;
+
+            // TRADUCTION EN TEMPS RÉEL
+            output.dispatchEvent(new Event("input"));
         }
 
         // SUPPRIMER
         if (e.target.classList.contains("delete-btn")) {
             output.value = output.value.slice(0, -1);
+            output.dispatchEvent(new Event("input"));
         }
     });
 }
